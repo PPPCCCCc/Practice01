4902ad6a2d61bce8d49e9fb643afc8c2a39be17c
ParkChanHee <ParkChanHee@210.123.107.251>
2022-04-19 10:16:37 +0900

    2

--- src/com/javaex/practice3/Ex11.java
+++ src/com/javaex/practice3/Ex11.java
@@ -1,19 +1,21 @@
 package com.javaex.practice3;
 import java.util.Scanner;
-public class Ex13 {
+public class Ex11 {
 
 	public static void main(String[] args) {
 		int a;
-		int b=0;
-	
+		int sum=0;
 		Scanner sc = new Scanner(System.in);
-		System.out.println("숫자를 입력하세요");
-		a =sc.nextInt();
 		
-		for(int i=1; i<=a; i++) {
-			b=b+i;
+		System.out.println("숫자를 입력하세요");
+		a = sc.nextInt();
+		
+		while(int i=a; i>a;i++) {
+			
+			
 		}
-		System.out.println("결과값 : " + b);
+		
+
 	}
 
 }
